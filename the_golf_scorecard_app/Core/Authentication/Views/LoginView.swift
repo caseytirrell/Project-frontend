3c0537fdd6589a78bd36e056bd3056b7c732fa4d
Casey Tirrell <tirrellcasey@gmail.com>
2024-03-10 12:59:40 -0400

    Added Field validation for login and registration views

--- the_golf_scorecard_app/Core/Authentication/Views/LoginView.swift
+++ the_golf_scorecard_app/Core/Authentication/Views/LoginView.swift
@@ -5,7 +5,32 @@ struct LoginView: View {
     @State private var showEmail = false
     @State private var password = ""
     @State private var showPassword = false
+    @State private var emailErrors: [String] = []
+    @State private var passwordErrors: [String] = []
     @EnvironmentObject var viewModel: AuthViewModel
+    
+    enum ValidationType {
+        case nonEmpty
+        case email
+    }
+    
+    func validateText(text: String, validationType: ValidationType) -> [String] {
+        
+        var errors: [String] = []
+        
+        switch validationType {
+        case .nonEmpty:
+            if text.isEmpty {
+                errors.append("This Field must be filled in.")
+            }
+        case .email:
+            if !text.contains("@") || !text.contains(".") {
+                errors.append("Please enter a valid email address")
+            }
+        }
+        return errors
+    }
+    
     var body: some View {
         NavigationStack{
             VStack {
@@ -20,8 +45,24 @@ struct LoginView: View {
                                 VStack(spacing: 24) {
                                     InputView(text: $email, title: "Email Address", placeholder: "yourname@example.com", showError: showEmail)
                                         .autocapitalization(.none)
+                                    if !emailErrors.isEmpty {
+                                        ForEach(emailErrors, id: \.self) {
+                                            error in
+                                            Text(error)
+                                                .foregroundColor(.red)
+                                                .font(.caption)
+                                        }
+                                    }
                                     
                                     InputView(text: $password, title: "Password", placeholder: "Enter Password", isSecureFiled: true, showError: showPassword)
+                                    if !passwordErrors.isEmpty {
+                                        ForEach(passwordErrors, id: \.self) {
+                                            error in
+                                            Text(error)
+                                                .foregroundColor(.red)
+                                                .font(.caption)
+                                        }
+                                    }
                                 }
                                 .padding(.all, 24)
                                 // log in button
@@ -29,8 +70,10 @@ struct LoginView: View {
                                     
                                     showEmail = email.isEmpty
                                     showPassword = password.isEmpty
+                                    emailErrors = validateText(text: email, validationType: .email)
+                                    passwordErrors = validateText(text: password, validationType: .nonEmpty)
                                     
-                                    guard !showEmail && !showPassword else { return }
+                                    guard emailErrors.isEmpty, passwordErrors.isEmpty, !showEmail, !showPassword else { return }
                                     
                                     Task {
                                         try await viewModel.logIn(withEmail: email, password: password)
